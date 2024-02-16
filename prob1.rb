b901e0b886041322228ef95f73e3e89d62ae8e18
Malcolm King <malcolm.elijah.king@gmail.com>
2024-02-15 19:47:38 -0600

    Added solution to prob2.rb

--- prob1.rb
+++ prob1.rb
@@ -5,3 +5,4 @@ end
 
 puts tag :p, "This is a paragraph"
 puts tag :h1, "This is a header"
+
--- prob2.rb
+++ prob2.rb
@@ -1,8 +1,14 @@
 numbers = [1, 2, 3, 4, 5]
-array = []
 
 numbers.each do |n|
     double = n * 2
     puts double
 end
 
+triple = Array.new(numbers.map{|n| n * 3})
+puts "#{triple}"
+
+
+'''
+array = numbers.map{|n| n * 3}
+'''
